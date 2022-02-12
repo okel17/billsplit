c55ec97ebf4ce754afdeb46a6efb00dca6d0e934
Om <om.u.kelkar@gmail.com>
2022-02-12 14:48:17 -0500

    Basic query using asyncgraphql.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::*;
+struct Query;
+
+#[Object]
+impl Query {
+    async fn make_email(&self, name: String) -> String {
+        name + "@gmail.com"
+    }
 }
+
+#[actix_web::main]
+async fn main() -> serde_json::Result<()> {
+
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    let res = schema.execute("{ makeEmail( name: \"om\" ) }").await;
+    
+    let json = serde_json::to_string(&res)?;
+    println!("{}", json);
+    
+    Ok(())
+}
+
 
